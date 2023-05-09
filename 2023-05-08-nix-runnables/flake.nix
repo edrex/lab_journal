cb96055986235cbac9f0b0dba9b3af309243cffc
Eric Drechsel <eric@pdxhub.org>
2023-05-09 16:27:12 -0700

    doc

--- 2023-05-08-nix-runnables/flake.nix
+++ 2023-05-08-nix-runnables/flake.nix
@@ -1,4 +1,6 @@
 {
+  # All about runnables.
+  # Source of truth: https://github.com/NixOS/nix/blob/master/tests/flakes/run.sh
   outputs = inputs@{ flake-parts, nixpkgs, ... }:
   flake-parts.lib.mkFlake { inherit inputs; } {
     systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
@@ -11,16 +13,14 @@
         name = "hello";
         text = ./cmd/hello;
       };
-      packages.pc = pkgs.writeShellApplication {
-        name = "";
-        text = ./cmd/hello;
-      };
+      # this one will fail tho - program name doesn't match
+      packages.pc = pkgs.writeScriptBin "hellno" (builtins.readFile ./cmd/hello);
       # Apps must have a structure like this.
-      # Why the distinction?
+      # Why the distinction? 🤷
       apps = {
         aa = {
           type = "app";
-          program = "${self'.packages.a}/bin/hello";
+          program = "${self'.packages.pa}/bin/hello";
         };
       };
     };
